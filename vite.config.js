7ef1a481b135640e5a2f7a60d3136a976f56cc80
Victor Gabriel <fortxoto13@gmail.com>
2023-11-07 23:37:17 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/PCREPB",
+    base: "https://victorgabriellts.github.io/PCREPB/",
     plugins: [react()],
 });
